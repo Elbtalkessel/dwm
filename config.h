813761c2bc7931f138b9cfa4e923c4c46da4d853
Elbtalkessel <me@0x00.life>
2021-05-16 15:40:26 +0200

    -feature: dirtomon; focusmon; tagmon

--- config.h
+++ config.h
@@ -110,9 +110,6 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
-/* commands */
-static const char *termcmd[]  = { TERMINAL, NULL };
-
 /*
  * Xresources preferences to load at startup
  */
