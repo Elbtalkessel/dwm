ff21c678b4d4731a5d9a7e70f9c6acf8b47e2f53
Elbtalkessel <me@0x00.life>
2021-05-16 03:45:28 +0200

    define alphas

--- config.h
+++ config.h
@@ -41,6 +41,11 @@ static Sp scratchpads[] = {
 	{"spterm",      spcmd1},
 	{"spcalc",      spcmd2},
 };
+static const unsigned int alphas[][3]      = {
+    /*               fg      bg        border     */
+    [SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+    [SchemeSel]  = { OPAQUE, baralpha, borderalpha },
+};
 
 /* tagging */
 static const char *tags[] = { "", "", "", " 4", " 5", " 6", " 7", "", "" };
